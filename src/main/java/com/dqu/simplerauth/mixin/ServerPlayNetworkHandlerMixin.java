1006119dc8343a180e6890e7cdbe6949412853e2
Dqu1J <mcpeytwot5@gmail.com>
2021-06-17 21:45:10 +0300

    Fix block desync

--- src/main/java/com/dqu/simplerauth/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/dqu/simplerauth/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,17 +3,21 @@ package com.dqu.simplerauth.mixin;
 import com.dqu.simplerauth.listeners.OnGameMessage;
 import com.dqu.simplerauth.listeners.OnPlayerAction;
 import com.dqu.simplerauth.listeners.OnPlayerMove;
+import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
-public class ServerPlayNetworkHandlerMixin {
+public abstract class ServerPlayNetworkHandlerMixin {
     @Inject(method = "onPlayerMove", at = @At("HEAD"), cancellable = true)
     public void onPlayerMove(PlayerMoveC2SPacket packet, CallbackInfo ci) {
         boolean canMove = OnPlayerMove.canMove((ServerPlayNetworkHandler) (Object) this);
@@ -24,9 +28,14 @@ public class ServerPlayNetworkHandlerMixin {
 
     @Inject(method = "onPlayerAction", at = @At("HEAD"), cancellable = true)
     public void onPlayerAction(PlayerActionC2SPacket packet, CallbackInfo ci) {
-        boolean canInteract = OnPlayerAction.canInteract((ServerPlayNetworkHandler) (Object) this);
+        ServerPlayNetworkHandler networkHandler = (ServerPlayNetworkHandler) (Object) this;
+        boolean canInteract = OnPlayerAction.canInteract(networkHandler);
         if (!canInteract) {
             ci.cancel();
+            BlockPos blockPos = packet.getPos();
+            ServerPlayerEntity player = networkHandler.getPlayer();
+            Packet packet1 = new BlockUpdateS2CPacket(player.world, blockPos);
+            networkHandler.sendPacket(packet1);
         }
     }
 
