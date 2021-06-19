da5e6318a76bd6e4833e1a226d730df41071f5f7
Dqu1J <mcpeytwot5@gmail.com>
2021-06-19 21:36:19 +0300

    Fixes #1 Dropping items desync

--- src/main/java/com/dqu/simplerauth/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/dqu/simplerauth/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,13 +3,16 @@ package com.dqu.simplerauth.mixin;
 import com.dqu.simplerauth.listeners.OnGameMessage;
 import com.dqu.simplerauth.listeners.OnPlayerAction;
 import com.dqu.simplerauth.listeners.OnPlayerMove;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
+
     @Inject(method = "onPlayerMove", at = @At("HEAD"), cancellable = true)
     public void onPlayerMove(PlayerMoveC2SPacket packet, CallbackInfo ci) {
         boolean canMove = OnPlayerMove.canMove((ServerPlayNetworkHandler) (Object) this);
@@ -30,15 +34,25 @@ public class ServerPlayNetworkHandlerMixin {
     public void onPlayerAction(PlayerActionC2SPacket packet, CallbackInfo ci) {
         ServerPlayNetworkHandler networkHandler = (ServerPlayNetworkHandler) (Object) this;
         boolean canInteract = OnPlayerAction.canInteract(networkHandler);
-        if (!canInteract) {
-            ci.cancel();
-            
+        if (canInteract) return;
+
+        ci.cancel();
+        ServerPlayerEntity player = networkHandler.getPlayer();
+
+        if (packet.getAction() == PlayerActionC2SPacket.Action.DROP_ITEM || packet.getAction() == PlayerActionC2SPacket.Action.DROP_ALL_ITEMS) {
+            /*
+                Updates players main hand slot to prevent desync
+                This action only gets triggered when dropping from the main hand
+             */
+            ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
+            Packet packet1 = new ScreenHandlerSlotUpdateS2CPacket(-2, player.getInventory().getSlotWithStack(stack), stack);
+            networkHandler.sendPacket(packet1);
+        } else {
             /*
                 Sends a block update packet to the client
                 Prevents desync between client and server when breaking or placing blocks
             */
             BlockPos blockPos = packet.getPos();
-            ServerPlayerEntity player = networkHandler.getPlayer();
             Packet packet1 = new BlockUpdateS2CPacket(player.world, blockPos);
             networkHandler.sendPacket(packet1);
         }
