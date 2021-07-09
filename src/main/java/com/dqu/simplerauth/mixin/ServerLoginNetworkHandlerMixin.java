19660443784101702cb3ab82e6e9bd67598a98ba
ByMartrixx <bymartrixx@gmail.com>
2021-07-09 14:12:01 -0400

    Add OnPlayerLogin event

--- src/main/java/com/dqu/simplerauth/mixin/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/com/dqu/simplerauth/mixin/ServerLoginNetworkHandlerMixin.java
@@ -1,8 +1,6 @@
 package com.dqu.simplerauth.mixin;
 
-import com.dqu.simplerauth.AuthMod;
-import com.dqu.simplerauth.managers.ConfigManager;
-import com.dqu.simplerauth.managers.DbManager;
+import com.dqu.simplerauth.listeners.OnPlayerLogin;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
@@ -17,16 +15,8 @@ public class ServerLoginNetworkHandlerMixin {
     @Shadow @Nullable GameProfile profile;
 
     @Redirect(method = "onHello", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isOnlineMode()Z"))
-    private boolean doAuthenticatePlayerWithMojang(MinecraftServer server) {
-        boolean forcedOnlineAuth = ConfigManager.getBoolean("forced-online-auth");
-        boolean optionalOnlineAuth = ConfigManager.getBoolean("optional-online-auth");
-        if (!server.isOnlineMode() || (!forcedOnlineAuth && !optionalOnlineAuth)) {
-            return false;
-        }
-
+    private boolean useOnlineAuth(MinecraftServer server) {
         //noinspection ConstantConditions - this.profile was set just before
-        String username = this.profile.getName();
-        if (forcedOnlineAuth) return AuthMod.doesMinecraftAccountExist(username);
-        else return DbManager.isPlayerRegistered(username) && DbManager.getUseOnlineAuth(username);
+        return OnPlayerLogin.canUseOnlineAuth(server, this.profile.getName());
     }
 }
