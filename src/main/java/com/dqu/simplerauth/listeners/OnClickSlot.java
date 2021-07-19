e28f0ae7d24b923410ad18df2d1711c61b34c6ab
Londiuh <gempleyero@gmail.com>
2021-07-19 14:31:33 +0200

    Cleanup

--- src/main/java/com/dqu/simplerauth/listeners/OnClickSlot.java
+++ src/main/java/com/dqu/simplerauth/listeners/OnClickSlot.java
@@ -2,7 +2,6 @@ package com.dqu.simplerauth.listeners;
 
 import com.dqu.simplerauth.AuthMod;
 import com.dqu.simplerauth.PlayerObject;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class OnClickSlot {
--- src/main/java/com/dqu/simplerauth/listeners/OnGameMessage.java
+++ src/main/java/com/dqu/simplerauth/listeners/OnGameMessage.java
@@ -2,8 +2,6 @@ package com.dqu.simplerauth.listeners;
 
 import com.dqu.simplerauth.AuthMod;
 import com.dqu.simplerauth.PlayerObject;
-import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class OnGameMessage {
