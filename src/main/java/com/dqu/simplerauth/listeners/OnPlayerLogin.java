b449d24f4a3be43d2c1769d4accf13e250dddeed
Dqu1J <mcpeytwot5@gmail.com>
2021-07-22 19:26:39 +0300

    Fixes #27 part 2

--- src/main/java/com/dqu/simplerauth/listeners/OnPlayerLogin.java
+++ src/main/java/com/dqu/simplerauth/listeners/OnPlayerLogin.java
@@ -9,10 +9,12 @@ public class OnPlayerLogin {
     public static boolean canUseOnlineAuth(MinecraftServer server, String username) {
         boolean forcedOnlineAuth = ConfigManager.getBoolean("forced-online-auth");
         boolean optionalOnlineAuth = ConfigManager.getBoolean("optional-online-auth");
+        boolean isGlobalAuth = ConfigManager.getAuthType().equals("global");
         if (!forcedOnlineAuth && !optionalOnlineAuth) {
             return false;
         }
 
+        if (isGlobalAuth) return false;
         if (forcedOnlineAuth) return !ConfigManager.forcePlayerOffline(username) && AuthMod.doesMinecraftAccountExist(username);
         else return DbManager.isPlayerRegistered(username) && DbManager.getUseOnlineAuth(username);
     }
