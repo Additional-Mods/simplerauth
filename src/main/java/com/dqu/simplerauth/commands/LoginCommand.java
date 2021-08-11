6c935a8c298f25df95bf5d6b9c5fe10dd0e66104
Londiuh <gempleyero@gmail.com>
2021-08-11 14:31:01 +0200

    Improve LoginCommand

--- src/main/java/com/dqu/simplerauth/commands/LoginCommand.java
+++ src/main/java/com/dqu/simplerauth/commands/LoginCommand.java
@@ -7,6 +7,9 @@ import com.dqu.simplerauth.managers.LangManager;
 import com.dqu.simplerauth.PlayerObject;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
@@ -18,61 +21,62 @@ public class LoginCommand {
     public static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("login")
             .then(argument("password", StringArgumentType.word())
-                .executes(ctx -> {
-                    String password = StringArgumentType.getString(ctx, "password");
-                    String username = ctx.getSource().getPlayer().getEntityName();
-                    ServerPlayerEntity player = ctx.getSource().getPlayer();
-                    PlayerObject playerObject = AuthMod.playerManager.get(player);
-                    String passwordtype = ConfigManager.getAuthType();
-
-                    if (playerObject.isAuthenticated()) {
-                        ctx.getSource().sendFeedback(LangManager.getLiteralText("command.login.alreadylogged"), false);
-                        return 1;
-                    }
-
-                    if (passwordtype.equals("local")) {
-                        // Local Password Authentication
-
-                        if (!DbManager.isPlayerRegistered(username)) {
-                            ctx.getSource().sendFeedback(LangManager.getLiteralText("command.login.notregistered"), false);
-                            return 1;
-                        }
-
-                        if (DbManager.isPasswordCorrect(username, password)) {
-                            playerObject.authenticate(player);
-                            if (ConfigManager.getBoolean("sessions-enabled"))
-                                DbManager.sessionCreate(player.getEntityName(), player.getIp());
-                            ctx.getSource().sendFeedback(LangManager.getLiteralText("command.general.authenticated"), false);
-                        } else {
-                            player.networkHandler.disconnect(LangManager.getLiteralText("command.general.notmatch"));
-                        }
-                    } else if (passwordtype.equals("global")) {
-                        // Global Password Authentication
-                        String globalPassword = ConfigManager.getString("global-password");
-
-                        if (password.equals(globalPassword)) {
-                            playerObject.authenticate(player);
-                            if (ConfigManager.getBoolean("sessions-enabled"))
-                                DbManager.sessionCreate(player.getEntityName(), player.getIp());
-                            ctx.getSource().sendFeedback(LangManager.getLiteralText("command.general.authenticated"), false);
-                        } else {
-                            player.networkHandler.disconnect(LangManager.getLiteralText("command.general.notmatch"));
-                        }
-                    } else {
-                        // Config setup is wrong, kick the player
-                        player.networkHandler.disconnect(LangManager.getLiteralText("config.incorrect"));
-                    }
-
-                    boolean hideposition = ConfigManager.getBoolean("hide-position");
-                    if (hideposition) {
-                        Vec3d pos = DbManager.getPosition(username);
-                        if (pos != null)
-                            player.requestTeleport(pos.getX(), pos.getY(), pos.getZ());
-                    }
-
-                    return 1;
-                })
+                .executes(ctx -> login(ctx))
             )
         );
+    }
+
+    private static int login(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+        ServerPlayerEntity player = ctx.getSource().getPlayer();
+        PlayerObject playerObject = AuthMod.playerManager.get(player);
+
+        if (playerObject.isAuthenticated()) {
+            throw new SimpleCommandExceptionType(LangManager.getLiteralText("command.login.alreadylogged")).create();
+        }
+
+        String password = StringArgumentType.getString(ctx, "password");
+        String username = player.getEntityName();
+        
+        if (!isPasswordCorrect(username, password)) {
+            // TODO: fails and kick after x fails
+            player.networkHandler.disconnect(LangManager.getLiteralText("command.general.notmatch"));
+            return 0;
+        }
+        playerObject.authenticate(player);
+        if (ConfigManager.getBoolean("sessions-enabled")) {
+            DbManager.sessionCreate(player.getEntityName(), player.getIp());
+        }
+        ctx.getSource().sendFeedback(LangManager.getLiteralText("command.general.authenticated"), false);
+        if (ConfigManager.getBoolean("hide-position")) {
+            Vec3d pos = DbManager.getPosition(username);
+            if (pos != null)
+                player.requestTeleport(pos.getX(), pos.getY(), pos.getZ());
+        }
+        return 1;
+    }
+
+    private static boolean isPasswordCorrect(String username, String password) throws CommandSyntaxException {
+        switch (ConfigManager.getAuthType()) {
+            case "local" -> {
+                // Local Password Authentication
+                if (!DbManager.isPlayerRegistered(username)) {
+                    throw new SimpleCommandExceptionType(LangManager.getLiteralText("command.login.notregistered")).create();
+                }
+                if (DbManager.isPasswordCorrect(username, password)) {
+                    return true;
+                }
+            }
+            case "global" -> {
+                // Global Password Authentication
+                if (password.equals(ConfigManager.getString("global-password"))) {
+                    return true;
+                }
+            }
+            default -> {
+                // Config setup is wrong
+                throw new SimpleCommandExceptionType(LangManager.getLiteralText("config.incorrect")).create();
+            }
+        }
+        return false;
     }
 }
